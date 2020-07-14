a3bed69f8decc15d038d479d992291df4dbf7785
trung.hv <trung.hv@isofhcare.com>
2020-07-14 21:54:22 +0700

    .

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import {showLoading, hideLoading} from 'components/Loading/LoadingComponent';
 import Container from 'components/Container';
 import FlashMessage from 'configs/FlashMessage';
 import {getlistMessage} from 'configs/apis/getListMessage';
 import NavigationServices from 'routes/NavigationServices';
 import screenName from 'configs/screenName';
+import FloatingLabelInput from 'components/FloatInput';
 
 const LoginScreen = () => {
+  const [userName, setUserName] = useState('');
   const onClick = () => {
     // showLoading();
     // getlistMessage(1, 1);
     // FlashMessage.show('message', 'danger', 6000);
-    NavigationServices.navigate(screenName.HOME)
+    NavigationServices.navigate(screenName.HOME);
   };
   return (
     <Container title={'Login'} style={styles.container}>
-      <Text>LOGIN</Text>
+      <FloatingLabelInput
+        label="Email"
+        value={this.state.value}
+        onChangeText={this.handleTextChange}
+      />
     </Container>
   );
 };
